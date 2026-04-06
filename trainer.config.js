106a69c718f0f10533e0c1b008c0607348d25774
Kevin Naranjo <kevinnm770@gmail.com>
2026-04-06 13:48:27 -0600

    first commit

---
+++ trainer.config.js
@@ -0,0 +1,19 @@
+// ─────────────────────────────────────────
+//  CONFIGURACIÓN DEL ENTRENADOR
+// ─────────────────────────────────────────
+
+const TRAINER = {
+  name:        "KEV",
+  hometown:    "Cartago",
+  catchphrase: "La cueva más oscura esconde la llama más feroz.",
+
+  favoritePokemon: "druddigon",
+  nickname:        "Drudi",
+  battleCry:       "¡GRAAAHHH! ¡Druddigon, arrasa con todo!",
+
+  definitiveMoveName:   "Stone Fire",
+  definitiveMoveFlavor: "Las rocas arden. El suelo se parte. No hay refugio cuando Drudi desata la tormenta de piedra y fuego.",
+
+  winMessage:  "Sabía que lo teníamos. Drudi nunca falla.",
+  loseMessage: "Esta vez no alcanzó... pero volveremos más fuertes.",
+};
